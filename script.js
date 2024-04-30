460ed6c68a2bedddbca12d97494b6189c327d6f4
codenoob66 <167981239+codenoob66@users.noreply.github.com>
2024-04-30 19:10:40 +0800

    update

--- script.js
+++ script.js
@@ -19,6 +19,32 @@ function pauseAudio() {
   document.getElementById("play_btn").innerText = "Play";
 }
 
+function checkAnswer() {
+  var answer1 = document.getElementById("answer1").value.toLowerCase();
+  if (answer1 === "bad blood" || answer1 === "badblood") {
+    alert("Correct!");
+  } else {
+    alert("Incorrect!");
+  }
+}
+
+document.getElementById("submitBtn1").addEventListener("click", function () {
+  checkAnswer(); // Call the checkAnswer function when the button is clicked
+});
+
+function checkAnswer2() {
+  var answer2 = document.getElementById("answer2").value.toLowerCase();
+  if (answer2 === "betty") {
+    alert("Correct!");
+  } else {
+    alert("Incorrect!");
+  }
+}
+
+document.getElementById("submitBtn2").addEventListener("click", function () {
+  checkAnswer2(); // Call the checkAnswer function when the button is clicked
+});
+
 function nextPage() {
   window.location.href = "answeringpage.html";
   alert("Continuing to the next page");
