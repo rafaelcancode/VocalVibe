37ac2020c07c5c7405c74b4c71d1216c065b229d
codenoob66 <167981239+codenoob66@users.noreply.github.com>
2024-04-30 18:36:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,31 @@
-$(document).ready(function() {
-    var btn = $(".button");
-    btn.click(function() {
-      btn.toggleClass("paused");
-      return false;
-    });
-  });
-
-
 function togglePlayPause() {
-    var audio = document.getElementById("audio-player");
-    if (audio.paused) {
-        playAudio();
-    } else {
-        pauseAudio();
-    }
+  var audio = document.getElementById("audio-player");
+  if (audio.paused) {
+    playAudio();
+  } else {
+    pauseAudio();
+  }
 }
 
 function playAudio() {
-    var audio = document.getElementById("audio-player");
-    audio.play();
-    document.getElementById("play_btn").innerText = "Pause";
+  var audio = document.getElementById("audio-player");
+  audio.play();
+  document.getElementById("play_btn").innerText = "Pause";
 }
 
 function pauseAudio() {
-    var audio = document.getElementById("audio-player");
-    audio.pause();
-    document.getElementById("play_btn").innerText = "Play";
+  var audio = document.getElementById("audio-player");
+  audio.pause();
+  document.getElementById("play_btn").innerText = "Play";
 }
 
 function nextPage() {
-    window.location.href = "answeringpage.html";
-    alert("Continuing to the next page");
+  window.location.href = "answeringpage.html";
+  alert("Continuing to the next page");
 }
 
 // Check if the element with id "continue-btn" exists
 var next = document.getElementById("continue-btn");
 if (next) {
-    next.addEventListener("click", nextPage);
+  next.addEventListener("click", nextPage);
 }
