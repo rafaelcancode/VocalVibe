44f7a6cd5f71dd9224ee0347d52479d473baa1f1
codenoob66 <167981239+codenoob66@users.noreply.github.com>
2024-04-30 00:23:28 -0700

    added some features

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
-var next = document.getElementById("continue-btn")
+$(document).ready(function() {
+    var btn = $(".button");
+    btn.click(function() {
+      btn.toggleClass("paused");
+      return false;
+    });
+  });
+
+
+function togglePlayPause() {
+    var audio = document.getElementById("audio-player");
+    if (audio.paused) {
+        playAudio();
+    } else {
+        pauseAudio();
+    }
+}
+
+function playAudio() {
+    var audio = document.getElementById("audio-player");
+    audio.play();
+    document.getElementById("play_btn").innerText = "Pause";
+}
+
+function pauseAudio() {
+    var audio = document.getElementById("audio-player");
+    audio.pause();
+    document.getElementById("play_btn").innerText = "Play";
+}
 
 function nextPage() {
     window.location.href = "answeringpage.html";
-    alert("continuing to next page")
+    alert("Continuing to the next page");
 }
 
-next.addEventListener("click", nextPage);
+// Check if the element with id "continue-btn" exists
+var next = document.getElementById("continue-btn");
+if (next) {
+    next.addEventListener("click", nextPage);
+}
